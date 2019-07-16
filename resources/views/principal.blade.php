df592b90ac6aa9ae315ada0f9a9397aad0b98ffd
AirelJaramillo96 <airel.jaramillo@cceo.com.mx>
2019-07-16 16:08:12 -0500

    Principal.blade refresh

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -8,13 +8,13 @@
     <meta name="description" content="Sistema Ventas Laravel Vue Js- IncanatoIT">
     <meta name="author" content="Incanatoit.com">
     <meta name="keyword" content="Sistema ventas Laravel Vue Js, Sistema compras Laravel Vue Js">
-    <link rel="shortcut icon" href="vendors/img/favicon.png">
+    <link rel="shortcut icon" href="img/favicon.png">
     <title>Sistema Ventas - IncanatoIT</title>
     <!-- Icons -->
-    <link href="vendors/css/font-awesome.min.css" rel="stylesheet">
-    <link href="vendors/css/simple-line-icons.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/simple-line-icons.min.css" rel="stylesheet">
     <!-- Main styles for this application -->
-    <link href="vendors/css/style.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
 </head>
 
 <body class="app header-fixed sidebar-fixed aside-menu-fixed aside-menu-hidden">
@@ -56,7 +56,7 @@
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                    <img src="vendors/img/avatars/6.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
+                    <img src="img/avatars/6.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
                     <span class="d-md-down-none">admin </span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right">
@@ -361,14 +361,14 @@
     </footer>
 
     <!-- Bootstrap and necessary plugins -->
-    <script src="vendors/js/jquery.min.js"></script>
-    <script src="vendors/js/popper.min.js"></script>
-    <script src="vendors/js/bootstrap.min.js"></script>
-    <script src="vendors/js/pace.min.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/pace.min.js"></script>
     <!-- Plugins and scripts required by all views -->
-    <script src="vendors/js/Chart.min.js"></script>
+    <script src="js/Chart.min.js"></script>
     <!-- GenesisUI main scripts -->
-    <script src="vendors/js/template.js"></script>
+    <script src="js/template.js"></script>
 </body>
 
 </html>
