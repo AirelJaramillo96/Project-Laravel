ff4cc29a0390755e54a06db9fab7aa52e8712915
AirelJaramillo96 <airel.jaramillo@cceo.com.mx>
2019-07-18 19:16:42 -0500

    Pagination

--- resources/assets/js/components/Category.vue
+++ resources/assets/js/components/Category.vue
@@ -75,23 +75,14 @@
                         </table>
                         <nav>
                             <ul class="pagination">
-                                <li class="page-item">
-                                    <a class="page-link" href="#">Ant</a>
+                                <li class="page-item" v-if="pagination.current_page > 1">
+                                    <a class="page-link" href="#" @click.prevent="changePage(pagination.current_page - 1)" >Ant</a>
                                 </li>
-                                <li class="page-item active">
-                                    <a class="page-link" href="#">1</a>
+                                <li class="page-item" v-for="page in pagesNumber" :key="page" :class="[page == isActived ? 'active' : '']">
+                                    <a class="page-link" href="#" @click.prevent="changePage(page)" v-text="page"></a>
                                 </li>
-                                <li class="page-item">
-                                    <a class="page-link" href="#">2</a>
-                                </li>
-                                <li class="page-item">
-                                    <a class="page-link" href="#">3</a>
-                                </li>
-                                <li class="page-item">
-                                    <a class="page-link" href="#">4</a>
-                                </li>
-                                <li class="page-item">
-                                    <a class="page-link" href="#">Sig</a>
+                                <li class="page-item" v-if="pagination.current_page < pagination.last_page">
+                                    <a class="page-link" href="#" @click.prevent="changePage(pagination.current_page + 1)">Sig</a>
                                 </li>
                             </ul>
                         </nav>
@@ -161,14 +152,50 @@
                tittlemodal : '',
                typeAction : 0,
                errorCategory : 0,
-               errorShowMsjCategory : []
+               errorShowMsjCategory : [],
+               pagination : {
+                   'total' : 0,
+                   'current_page' : 0,
+                   'per_page' : 0,
+                   'last_page' : 0,
+                   'from' : 0,
+                   'to' : 0,
+               },
+               offset : 3
            } 
         },
+        computed:{
+            isActived: function(){
+                return this.pagination.current_page;
+            },
+            pagesNumber: function(){
+                if(!this.pagination.to){
+                    return [];
+            }
+            var from = this.pagination.current_page = this.offset;
+            if (from < 1 ){
+                from = 1;
+            }
+            var to = from + (this.offset * 2);
+            if (to >= this.pagination.last_page){
+                to = this.pagination.last_page;
+            }
+            var pagesArray = [];
+            while (from <= to){
+                pagesArray.push(from);
+                from++;
+            }
+            return pagesArray;
+            }
+        },
         methods : {
-            listCategory (){
+            listCategory (page){
                 let me=this;
-                axios.get('/category').then(function (response) {
-                    me.arrayCategory = response.data;
+                var url = '/category?page=' + page;
+                axios.get(url).then(function (response) {
+                    var respuesta= response.data;
+                    me.arrayCategory = respuesta.categories.data;
+                    me.pagination= respuesta.pagination;
                 // handle success
                 console.log(response);
                 })
@@ -176,6 +203,13 @@
                 // handle error
                  console.log(error);
                 });
+            },
+            changePage (page){
+                let me = this;
+                //Actualizar pagina actual
+                me.pagination.current_page = page;
+                //Envia la petición para vizualizar la data desde esa pagina 
+                me.listCategory(page);
             },
             registerCategory(){
                 if (this.valideCategory()){
