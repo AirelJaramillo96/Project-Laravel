c0df3f2cf775884e08780b2e98044dbd9d85b610
AirelJaramillo96 <airel.jaramillo@cceo.com.mx>
2019-07-18 22:50:29 -0500

    Pagination 2

--- public/js/app.js
+++ public/js/app.js
@@ -50470,21 +50470,21 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
     },
 
     computed: {
-        isActived: function isActived() {
-            return this.pagination.current_page;
-        },
         pagesNumber: function pagesNumber() {
             if (!this.pagination.to) {
                 return [];
             }
-            var from = this.pagination.current_page = this.offset;
+
+            var from = this.pagination.current_page - this.offset;
             if (from < 1) {
                 from = 1;
             }
+
             var to = from + this.offset * 2;
             if (to >= this.pagination.last_page) {
                 to = this.pagination.last_page;
             }
+
             var pagesArray = [];
             while (from <= to) {
                 pagesArray.push(from);
--- resources/assets/js/components/Category.vue
+++ resources/assets/js/components/Category.vue
@@ -76,7 +76,7 @@
                         <nav>
                             <ul class="pagination">
                                 <li class="page-item" v-if="pagination.current_page > 1">
-                                    <a class="page-link" href="#" @click.prevent="changePage(pagination.current_page - 1)" >Ant</a>
+                                    <a class="page-link" href="#" @click.prevent="changePage(pagination.current_page - 1)">Ant</a>
                                 </li>
                                 <li class="page-item" v-for="page in pagesNumber" :key="page" :class="[page == isActived ? 'active' : '']">
                                     <a class="page-link" href="#" @click.prevent="changePage(page)" v-text="page"></a>
@@ -165,27 +165,27 @@
            } 
         },
         computed:{
-            isActived: function(){
-                return this.pagination.current_page;
-            },
-            pagesNumber: function(){
-                if(!this.pagination.to){
+            pagesNumber: function() {
+                if(!this.pagination.to) {
                     return [];
-            }
-            var from = this.pagination.current_page = this.offset;
-            if (from < 1 ){
-                from = 1;
-            }
-            var to = from + (this.offset * 2);
-            if (to >= this.pagination.last_page){
-                to = this.pagination.last_page;
-            }
-            var pagesArray = [];
-            while (from <= to){
-                pagesArray.push(from);
-                from++;
-            }
-            return pagesArray;
+                }
+                
+                var from = this.pagination.current_page - this.offset; 
+                if(from < 1) {
+                    from = 1;
+                }
+
+                var to = from + (this.offset * 2); 
+                if(to >= this.pagination.last_page){
+                    to = this.pagination.last_page;
+                }  
+
+                var pagesArray = [];
+                while(from <= to) {
+                    pagesArray.push(from);
+                    from++;
+                }
+                return pagesArray;
             }
         },
         methods : {
